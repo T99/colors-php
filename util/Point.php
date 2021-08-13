7db37f22766033892da75380b7e9a2ee9059675c
Trevor Sears <trevors@knorrmarketing.com>
2021-08-13 10:27:26 -0400

    Added basic `Point` class to support new `forEachPixel` method.

---
+++ util/Point.php
@@ -0,0 +1,22 @@
+<?php
+
+class Point {
+	
+	/**
+	 * @var int
+	 */
+	public $x;
+	
+	/**
+	 * @var int
+	 */
+	public $y;
+	
+	public function __construct(int $x, int $y) {
+		
+		$this->x = $x;
+		$this->y = $y;
+		
+	}
+	
+}
