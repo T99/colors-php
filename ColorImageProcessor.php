41eb7184ea191bb9a6773cc9b2392e8baa72ef2a
Trevor Sears <trevors@knorrmarketing.com>
2021-08-13 17:00:15 -0400

    Lots of refactoring in `ColorImageProcessor` to support float densities and remote images.

--- ColorImageProcessor.php
+++ ColorImageProcessor.php
@@ -1,23 +1,70 @@
 <?php
 
+include_once "./util/Point.php";
+include_once "./util/density.php";
 include_once "./ColorCollection.php";
 
 class ColorImageProcessor {
 	
+	/**
+	 * @var string
+	 */
 	protected $imagePath;
 	
-	public function __construct(string $imagePath) {
+	/**
+	 * @var Imagick
+	 */
+	protected $image;
 	
-		if (file_exists($imagePath)) $this->imagePath = $imagePath;
+	protected function __construct(string $imagePath) {
+	
+		$this->imagePath = $imagePath;
+	
+	}
+	
+	public static function fromURI(string $uri): ColorImageProcessor {
+		
+		if (substr($uri, 0, 4) === "http") return ColorImageProcessor::fromRemoteFile($uri);
+		else return ColorImageProcessor::fromLocalFilePath($uri);
+		
+	}
+	
+	public static function fromLocalFilePath(string $imagePath): ColorImageProcessor {
+		
+		$result = new ColorImageProcessor($imagePath);
+		
+		if (file_exists($imagePath)) $result->imagePath = $imagePath;
 		else throw new Error("Could not find the image file located at: '$imagePath'...");
+		
+		$result->image = new \Imagick(realpath($result->imagePath));
+		
+		return $result;
+		
+	}
 	
+	public static function fromRemoteFile(string $imagePath): ColorImageProcessor {
+		
+		$tempFile = tmpfile();
+		$tempFileMetaData = stream_get_meta_data($tempFile);
+		$tempFileURI = $tempFileMetaData["uri"];
+		
+		$remoteImage = file_get_contents($imagePath);
+		file_put_contents($tempFileURI, $remoteImage);
+		
+		return ColorImageProcessor::fromLocalFilePath($tempFileURI);
+		
+	}
+	
+	public function getImageSize(): Point {
+		
+		return new Point($this->image->getImageWidth(), $this->image->getImageHeight());
+		
 	}
 	
 	public function forEachPixel(callable $consumer): void {
 		
-		$image = new \Imagick(realpath($this->imagePath));
-		$pixelRowIterator = $image->getPixelIterator();
-		$imageSize = new Point($image->getImageWidth(), $image->getImageHeight());
+		$pixelRowIterator = $this->image->getPixelIterator();
+		$imageSize = $this->getImageSize();
 		
 		foreach ($pixelRowIterator as $rowIndex => $pixelRow) {
 			
@@ -25,7 +72,7 @@ class ColorImageProcessor {
 				
 				$coordinates = new Point($columnIndex, $rowIndex);
 				
-				$consumer($imageSize, $coordinates, $image->getImagePixelColor($coordinates->x, $coordinates->y));
+				$consumer($imageSize, $coordinates, $this->image->getImagePixelColor($coordinates->x, $coordinates->y));
 				
 			}
 			
@@ -36,43 +83,106 @@ class ColorImageProcessor {
 	/**
 	 * Returns a ColorCollection object containing all of the strictly distinct colors found in the input image.
 	 *
-	 * @param int $density The 'absolute density' at pixels will be extracted from the image. An absolute density of 'n'
+	 * @param float $absoluteDensity The 'absolute density' at pixels will be extracted from the image. An absolute density of 'n'
 	 * means that a pixel will be extracted from the intersection of every n columns and n rows.
 	 * @return ColorCollection A ColorCollection object containing all of the strictly distinct colors found in the
 	 * input image at the specified density.
 	 */
-	public function getDistinctColorsByAbsoluteDensity(int $density = 1): ColorCollection {
+	public function getDistinctColorsByAbsoluteDensity(float $absoluteDensity = 1): ColorCollection {
 		
+		$imageSize = $this->getImageSize();
 		$result = new ColorCollection();
 		
-		$this->forEachPixel(function (Point $imageSize, Point $pixelCoordinates, ImagickPixel $pixel)
-		                        use ($density, &$result): void {
+		for ($xCoordinate = 0; $xCoordinate < $imageSize->x; $xCoordinate += $absoluteDensity) {
 			
-			if (($pixelCoordinates->x % $density === 0) && ($pixelCoordinates->y % $density === 0)) {
+			$realXCoordinate = round($xCoordinate);
+			
+			for ($yCoordinate = 0; $yCoordinate < $imageSize->y; $yCoordinate += $absoluteDensity) {
 				
-				$result->addColors(Color::fromRGB($pixel->getColorAsString()));
+				$realYCoordinate = round($yCoordinate);
+				
+				$result->addColors(
+					Color::fromRGB(
+						$this->image->getImagePixelColor($realXCoordinate, $realYCoordinate)->getColorAsString()
+					)
+				);
 				
 			}
 			
-		});
+		}
 		
 		return $result;
 	
 	}
 	
+	public function getDistinctColorsByRelativeDensity(float $relativeDensity): ColorCollection {
+		
+		return $this->getDistinctColorsByAbsoluteDensity(
+			relativeDensityToAbsoluteDensity($relativeDensity)
+		);
+		
+	}
+	
+	public function getDistinctColorsByMaximumSampleSize(int $maxSampleSize): ColorCollection {
+		
+		$imageSize = $this->getImageSize();
+		$totalPixelCount = $imageSize->x * $imageSize->y;
+		
+		return $this->getDistinctColorsByRelativeDensity($maxSampleSize / $totalPixelCount);
+		
+	}
+	
+	public function getPrimaryImageColors(): ColorCollection {
+		
+		// Get up to 50,000 color samples from the image.
+		$result = $this->getDistinctColorsByMaximumSampleSize(50000);
+		
+		echo "<pre>";
+		echo "background: " . $this->guessBackgroundColor();
+		echo "</pre>";
+		
+		// Attempt to remove colors that originated from the background.
+		$result = $result->getFilteredColorCollection(
+			ColorCollectionFilterFunctions::createEquivalencyFilter(
+				$this->guessBackgroundColor(),
+				ColorEquivalencyFunctions::createCIE94DeltaEEquivalencyFunction(30)
+			),
+			true
+		);
+		
+		// Sort the results so that the upcoming merge operation
+//		$result->sortByIncidence();
+		
+		// Use a weighted merge to merge colors that are found to have a CIE94 delta-E of 30 or less.
+//		$result = $result->getMergedColorCollection(
+//			ColorEquivalencyFunctions::createCIE94DeltaEEquivalencyFunction(30),
+//			ColorMergerFunctions::createWeightedAverageMergerFunction()
+//		);
+		
+		// Limit the results to only colors that comprise at least %5 of the colorspace.
+//		$result = $result->getFilteredColorCollection(
+//			ColorCollectionFilterFunctions::createRelativeOccurrenceFilter(0.05)
+//		);
+		
+		// Sort the result set by incidence in descending order.
+		$result->sortByIncidence();
+		
+		return $result;
+		
+	}
+	
 	public function guessBackgroundColor(): Color {
 		
-		$image = new \Imagick(realpath($this->imagePath));
 		$colorCollection = new ColorCollection();
 		
-		$imageWidth = $image->getImageWidth();
-		$imageHeight = $image->getImageHeight();
+		$imageWidth = $this->image->getImageWidth();
+		$imageHeight = $this->image->getImageHeight();
 		
 		$pixels = [
-			$image->getImagePixelColor(0, 0),
-			$image->getImagePixelColor($imageWidth, 0),
-			$image->getImagePixelColor(0, $imageHeight),
-			$image->getImagePixelColor($imageWidth, $imageHeight)
+			$this->image->getImagePixelColor(0, 0),
+			$this->image->getImagePixelColor($imageWidth, 0),
+			$this->image->getImagePixelColor(0, $imageHeight),
+			$this->image->getImagePixelColor($imageWidth, $imageHeight)
 		];
 		
 		$colorCollection->addColors(...array_map(function(ImagickPixel $pixel): Color {
