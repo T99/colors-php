c18d35eba500dccc7a4a5b30fa08a2c1afa10a40
Trevor Sears <trevors@knorrmarketing.com>
2021-08-16 15:15:39 -0400

    Added methods for interacting with local test DB in `db.php`.

--- util/db.php
+++ util/db.php
@@ -28,3 +28,48 @@ function getImageURLsFromDatabase(mysqli $dbc, int $limit = 20, int $offset = 0)
 	return $arrayResults;
 
 }
+
+function getImagesToConvert(mysqli $dbc, int $limit = 20, int $offset = 0): array {
+	
+	$brandName = "ekornes";
+	
+	$queryResult = $dbc->query("
+		SELECT `uri` FROM `imagesToConvert` LIMIT $limit OFFSET $offset
+	");
+	
+	$arrayResults = [];
+	
+	while ($row = $queryResult->fetch_assoc()) {
+		
+		$path = $row["uri"];
+		
+		$arrayResults[] = "https://knorrcatalog.s3.amazonaws.com$path";
+		
+	}
+	
+	return $arrayResults;
+	
+}
+
+function setImageColorsInDatabase(mysqli $dbc, string $url, ColorCollection &$colors): void {
+	
+	$totalCount = $colors->getOccurrenceCount();
+	
+	foreach ($colors as $color) {
+		
+		$colorID = $color->getColor()->toHexString(false);
+		$composition = number_format(count($color) / $totalCount, 3, '.', '');
+		
+		$dbc->query("
+			INSERT INTO `imageColors` (`uri`, `color`, `composition`)
+				VALUES ('$url', '$colorID', $composition)
+				ON DUPLICATE KEY UPDATE `composition` = $composition
+		");
+		
+		$dbc->query("
+			DELETE FROM `imagesToConvert` WHERE `uri` = '$colorID'
+		");
+		
+	}
+
+}
